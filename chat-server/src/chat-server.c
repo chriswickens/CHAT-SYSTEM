ce2567547c6500f4e794012e0c93d85d9f2d5429
chriswickens <chriswickens@gmail.com>
2025-03-26 15:35:35 -0400

    Made some comments to detail information

--- chat-server/src/chat-server.c
+++ chat-server/src/chat-server.c
@@ -14,19 +14,19 @@
 #define MAX_MESSAGE_SIZE 80
 
 // Function prototypes
-int init_listening_socket();
-void accept_new_connection(int listeningSocket, struct pollfd socketList[], int *totalSocketCount);
-void broadcast_message(struct pollfd socketList[], int totalSocketCount, char *messageToBroadcast);
-void handle_client_data(struct pollfd socketList[], int totalSocketCount);
+int initializeListener();
+void acceptConnection(int listeningSocket, struct pollfd socketList[], int *totalSocketCount);
+void broadcastChatMessage(struct pollfd socketList[], int totalSocketCount, char *messageToBroadcast);
+void processClientMessage(struct pollfd socketList[], int totalSocketCount);
 
 int main()
 {
     int listeningSocket, totalSocketCount = 1;
-    struct pollfd socketList[MAX_CLIENTS + 1]; // socketList[0] is for the listening socket
-    int timeout = 5000;                        // poll timeout in milliseconds
+    struct pollfd socketList[MAX_CLIENTS + 1]; // socketList[0] is for the listening socket (+1 for max clients AND the listener socket)
+    int timeout = 5000; // poll timeout in milliseconds
 
     // Initialize the listening socket.
-    listeningSocket = init_listening_socket();
+    listeningSocket = initializeListener();
     // Assign the listening socket to index 0 of the socket array
     socketList[0].fd = listeningSocket;
 
@@ -37,17 +37,17 @@ int main()
         socketList[i].fd = -1; // Mark remaining slots as unused.
     }
 
-    printf("Server listening on port %d\n", PORT);
+    printf("Server listening on port %d\numberOfBytesRead", PORT);
 
     while (1)
     {
-        int ret = poll(socketList, totalSocketCount, timeout);
-        if (ret < 0)
+        int returnCode = poll(socketList, totalSocketCount, timeout);
+        if (returnCode < 0)
         {
-            perror("poll failed");
+            perror("Error during polling!");
             break;
         }
-        if (ret == 0)
+        if (returnCode == 0)
         {
             // Timeout: no events.
             continue;
@@ -56,102 +56,176 @@ int main()
         // Accept new connections if available.
         if (socketList[0].revents & POLLIN)
         {
-            accept_new_connection(listeningSocket, socketList, &totalSocketCount);
+            acceptConnection(listeningSocket, socketList, &totalSocketCount);
         }
 
         // Process data from connected clients.
-        handle_client_data(socketList, totalSocketCount);
+        processClientMessage(socketList, totalSocketCount);
     }
 
     close(listeningSocket);
     return 0;
 }
 
-int init_listening_socket()
+
+// This code does not let the server use an address that has already been bound
+// If this is used, when you CTRL+C the server terminal, you need to wait 5000ms for the server
+// to stop listening on the socket
+// int initializeListener()
+// {
+//     int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+//     if (listenSocket < 0)
+//     {
+//         perror("socket failed");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     // Set the socket to non-blocking mode.
+//     int flags = fcntl(listenSocket, F_GETFL, 0);
+//     if (flags < 0)
+//     {
+//         perror("fcntl F_GETFL failed");
+//         exit(EXIT_FAILURE);
+//     }
+//     fcntl(listenSocket, F_SETFL, flags | O_NONBLOCK);
+
+//     // Prepare the server address.
+//     struct sockaddr_in serverAddress;
+//     memset(&serverAddress, 0, sizeof(serverAddress));
+//     serverAddress.sin_family = AF_INET;
+//     serverAddress.sin_addr.s_addr = INADDR_ANY;
+//     serverAddress.sin_port = htons(PORT);
+
+//     if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+//     {
+//         perror("bind failed");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     if (listen(listenSocket, MAX_CLIENTS) < 0)
+//     {
+//         perror("listen failed");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     return listenSocket;
+// }
+
+// Refined version of the above that lets the server bind to the an address already in use
+int initializeListener()
 {
-    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listen_fd < 0)
+    int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket < 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
+    // Allow reusing the address immediately after the server terminates.
+    int socketOption = 1;
+    if (setsockopt(listenSocket, SOL_SOCKET, SO_REUSEADDR, &socketOption, sizeof(socketOption)) < 0)
+    {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
     // Set the socket to non-blocking mode.
-    int flags = fcntl(listen_fd, F_GETFL, 0);
+    int flags = fcntl(listenSocket, F_GETFL, 0);
     if (flags < 0)
     {
-        perror("fcntl F_GETFL failed");
+        perror("Error getting flags!");
         exit(EXIT_FAILURE);
     }
-    fcntl(listen_fd, F_SETFL, flags | O_NONBLOCK);
+    fcntl(listenSocket, F_SETFL, flags | O_NONBLOCK);
 
     // Prepare the server address.
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT);
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
 
-    if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
     {
-        perror("bind failed");
+        perror("socket binding failed!");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(listen_fd, MAX_CLIENTS) < 0)
+    if (listen(listenSocket, MAX_CLIENTS) < 0)
     {
-        perror("listen failed");
+        perror("socket listen failed!");
         exit(EXIT_FAILURE);
     }
 
-    return listen_fd;
+    return listenSocket;
 }
 
-void accept_new_connection(int listen_fd, struct pollfd socketList[], int *nfds)
+// Accepts the connection from a client
+void acceptConnection(int listenSocket, struct pollfd socketList[], int *numberOfSockets)
 {
-    struct sockaddr_in cli_addr;
-    socklen_t cli_len = sizeof(cli_addr);
-    int conn_fd = accept(listen_fd, (struct sockaddr *)&cli_addr, &cli_len);
-    if (conn_fd < 0)
+    // Struct to hold the client address details
+    struct sockaddr_in clientAddress;
+
+    // The length in bytes of the address
+    socklen_t clientStructLength = sizeof(clientAddress);
+
+    // Get the result of the accept() function
+    int connectionResult = accept(listenSocket, (struct sockaddr *)&clientAddress, &clientStructLength);
+
+    if (connectionResult < 0)
     {
         if (errno != EWOULDBLOCK && errno != EAGAIN)
-            perror("accept failed");
+            perror("accept connection failed!");
         return;
     }
 
     // Set the new client socket to non-blocking.
-    int flags = fcntl(conn_fd, F_GETFL, 0);
-    fcntl(conn_fd, F_SETFL, flags | O_NONBLOCK);
+    int flags = fcntl(connectionResult, F_GETFL, 0);
+    fcntl(connectionResult, F_SETFL, flags | O_NONBLOCK);
 
     // Add the new connection to the pollfd array.
+    // Declare the for loop iterator outside of the loop to allow access further down
     int i;
+    
+    // Start at 1 to skip the listener socket in the server
     for (i = 1; i <= MAX_CLIENTS; i++)
     {
         if (socketList[i].fd < 0)
         {
-            socketList[i].fd = conn_fd;
+            socketList[i].fd = connectionResult;
             socketList[i].events = POLLIN;
-            if (i >= *nfds)
+            
+            // If the current number of sockets is greater than or equal to the actual number of sockets
+            // Increment number of sockets to represent a new connection
+            if (i >= *numberOfSockets)
             {
-                *nfds = i + 1;
+                *numberOfSockets = i + 1;
             }
-            printf("New connection, fd: %d\n", conn_fd);
+
+            // DEBUG
+            printf("DEBUG acceptConnection() : New connection, fd: %d\numberOfBytesRead", connectionResult);
             break;
         }
     }
+
+    // If the max clients will be exceeded, close the connection to the client trying to connect
     if (i > MAX_CLIENTS)
     {
         // Too many connections.
-        printf("Maximum clients reached. Rejecting connection.\n");
-        close(conn_fd);
+        printf("DEBUG acceptConnection() : Maximum clients reached. Rejecting connection.\numberOfBytesRead");
+        close(connectionResult);
     }
 }
 
-void broadcast_message(struct pollfd socketList[], int nfds, char *messageToBroadcast)
+// Sends the messages to the sockets of each connected client
+void broadcastChatMessage(struct pollfd socketList[], int numberOfSockets, char *messageToBroadcast)
 {
+    // This is where we will need to implement the protocol parsing to send a message containing
+    // necessary information back to all the clients
     // Simulate retrieving a user name.
     char userName[10] = "Wickens";
     int nameLength = strlen(userName);
+
     // Allocate space for the combined messageToBroadcast: username, ": ", messageToBroadcast, and null terminator.
     char combinedMessage[MAX_MESSAGE_SIZE + nameLength + 3];
 
@@ -161,35 +235,41 @@ void broadcast_message(struct pollfd socketList[], int nfds, char *messageToBroa
 
     int combinedLength = strlen(combinedMessage);
 
-    for (int j = 1; j < nfds; j++)
+    // Iterate over the client sockets (start at 1 to skip server listener socket)
+    for (int i = 1; i < numberOfSockets; i++)
     {
-        if (socketList[j].fd >= 0)
+        if (socketList[i].fd >= 0)
         {
-            int sent = send(socketList[j].fd, combinedMessage, combinedLength, 0);
+            int sent = send(socketList[i].fd, combinedMessage, combinedLength, 0);
+
+            // If NO messages were sent, there was a problemo!
             if (sent < 0)
             {
-                perror("send failed");
+                perror("DEBUG broadcastChatMessage() : send failed");
             }
         }
     }
 }
 
-void handle_client_data(struct pollfd socketList[], int nfds)
+void processClientMessage(struct pollfd socketList[], int numberOfSockets)
 {
     char buffer[MAX_MESSAGE_SIZE];
-    for (int i = 1; i < nfds; i++)
+
+    // Iterate over the client sockets (start at 1 to skip server listener socket)
+    for (int i = 1; i < numberOfSockets; i++)
     {
         if (socketList[i].fd < 0)
+        {
             continue;
+        }
 
         if (socketList[i].revents & POLLIN)
         {
-            int n = read(socketList[i].fd, buffer, MAX_MESSAGE_SIZE - 1);
+            int numberOfBytesRead = read(socketList[i].fd, buffer, MAX_MESSAGE_SIZE - 1);
 
-
-            if (n > 0)
+            if (numberOfBytesRead > 0)
             {
-                buffer[n] = '\0'; // Null-terminate the string.
+                buffer[numberOfBytesRead] = '\0'; // Null-terminate the string.
 
                 // Check for a specific messageToBroadcast (e.g., "quit") to remove the client.
                 // THIS NEEDS TO BE PUT INTO A FUNCTION TO CLEAN UP THE SOCKET LIST
@@ -197,20 +277,20 @@ void handle_client_data(struct pollfd socketList[], int nfds)
                 // Returns an INT that dictates if it succeeded or failed
                 if (strcmp(buffer, "quit") == 0)
                 {
-                    printf("Client on fd %d requested disconnect.\n", socketList[i].fd);
+                    printf("DEBUG processClientMessage() : Client on fd %d requested disconnect.\numberOfBytesRead", socketList[i].fd);
                     close(socketList[i].fd);
                     socketList[i].fd = -1;
                 }
 
                 else
                 {
-                    broadcast_message(socketList, nfds, buffer);
+                    broadcastChatMessage(socketList, numberOfSockets, buffer);
                 }
             }
-            else if (n == 0)
+            else if (numberOfBytesRead == 0)
             {
                 // Client disconnected.
-                printf("Client on fd %d disconnected.\n", socketList[i].fd);
+                printf("Client on fd %d disconnected.\numberOfBytesRead", socketList[i].fd);
                 close(socketList[i].fd);
                 socketList[i].fd = -1;
             }
--- makefile
+++ makefile
@@ -1,8 +1,15 @@
+.PHONY: all clean
+
+# The top-level "all" target calls the makefiles in the subdirectories.
 all:
 	$(MAKE) -C chat-client
 	$(MAKE) -C chat-server
+# Uncomment the next line for Common
+# $(MAKE) -C Common
 
+# The top-level "clean" target cleans all subdirectories.
 clean:
 	$(MAKE) -C chat-client clean
 	$(MAKE) -C chat-server clean
-
+# Uncomment the next line for common
+# $(MAKE) -C Common clean
