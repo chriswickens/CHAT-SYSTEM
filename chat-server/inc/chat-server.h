8bbc0190a5febc1ceda53ca7679ac48c5ff701cc
chriswickens <chriswickens@gmail.com>
2025-03-30 16:38:27 -0400

    updated server header

--- chat-server/inc/chat-server.h
+++ chat-server/inc/chat-server.h
@@ -11,16 +11,16 @@ Contains common functions and variables used by various programs in this system
 #ifndef CHAT_SERVER_H
 #define CHAT_SERVER_H
 
-// #include <fcntl.h>
 #include "../../Common/inc/common.h"
- // Function prototypes.
- int initializeListener();
- void acceptConnection(int listeningSocket);
- void broadcastChatMessage(char *messageToBroadcast, int senderSocket);
- void processClientMessage(int clientSocket);
- void *clientHandler(void *clientSocketPointer);
 
- // defines
+// Function prototypes.
+int initializeListener();
+void acceptConnection(int listeningSocket);
+void broadcastChatMessage(char *messageToBroadcast, int senderSocket);
+void processClientMessage(int clientSocket);
+void *clientHandler(void *clientSocketPointer);
+
+// defines
 #define MAX_CLIENTS 10
 
 #endif // CHAT_SERVER_H
