99b23d2545d8a4cd451760ee2fdc24e901b42600
chriswickens <chriswickens@gmail.com>
2025-03-30 15:20:07 -0400

    client funciton heads in H files

--- chat-client/inc/chat-client.h
+++ chat-client/inc/chat-client.h
@@ -6,12 +6,24 @@
 // #include <netdb.h>
 
 #include "../../Common/inc/common.h"
-
+void initializeNcursesWindows(void);
+int connectToServer(const char *serverIpAddress);
+void *handleReceivedMessage(void *arg);
+int startReceivingThread(void);
+void handleUserInput(char *userName, char *clientIP);
+void cleanup(void);
+void getLocalIP(char *ipBuffer, size_t bufferSize);
+void splitMessage(const char *fullString, char *firstPart, char *secondPart);
+void checkHostName(int hostname);
+void checkHostEntryDetails(struct hostent *hostentry);
+void ipAddressFormatter(char *IPbuffer);
+void sendProtocolMessage(const char *message);
+void updateUserInputWindow(WINDOW *inputWin, const char *currentBuffer, int userInputIndex);
 
 // Code here
 #define CLIENT_INPUT_MARKER ">"
 
-#define TOO_FEW_ARGUMENTS "ERROR: Too few arguments!\n"
+
 
 #define CLIENT_MAX_MSG_SIZE 81 // Message size used for MAX in client
 #define CLIENT_MSG_PART_LENGTH 40
--- chat-client/src/chat-client.c
+++ chat-client/src/chat-client.c
@@ -1,5 +1,3 @@
-
-
 #include "../inc/chat-client.h"
 
 
@@ -20,19 +18,7 @@ char clientIP[256]; // Stores the client's IP address
 // These belong in the chat-client.h file
 
 // SOME FUNCTION PROTOTYPES ARE MISSING!
-void initializeNcursesWindows(void);
-int connectToServer(const char *serverIpAddress);
-void *handleReceivedMessage(void *arg);
-int startReceivingThread(void);
-void handleUserInput(char *userName, char *clientIP);
-void cleanup(void);
-void getLocalIP(char *ipBuffer, size_t bufferSize);
-void splitMessage(const char *fullString, char *firstPart, char *secondPart);
-void checkHostName(int hostname);
-void checkHostEntryDetails(struct hostent *hostentry);
-void ipAddressFormatter(char *IPbuffer);
-void sendProtocolMessage(const char *message);
-void updateUserInputWindow(WINDOW *inputWin, const char *currentBuffer, int userInputIndex);
+
 
 void getLocalIP(char *ipBuffer, size_t bufferSize)
 {
